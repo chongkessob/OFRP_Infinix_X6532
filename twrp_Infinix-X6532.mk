f29e2197ca49e644bab05fd27fe423b2ea434d83
chongkessob <chonks091004@gmail.com>
2025-02-04 20:49:33 +0800

    Update and rename omni_Infinix-X6532.mk to twrp_Infinix-X6532.mk

--- twrp_Infinix-X6532.mk
+++ twrp_Infinix-X6532.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/infinix/Infinix-X6532/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6532
-PRODUCT_NAME := omni_Infinix-X6532
+PRODUCT_NAME := twrp_Infinix-X6532
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6532
 PRODUCT_MANUFACTURER := infinix
