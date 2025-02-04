9292f3d603cbb1431e6ea4b75a6fa67132e411d8
chongkessob <chonks091004@gmail.com>
2025-02-04 20:50:57 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X6532-user
-add_lunch_combo omni_Infinix-X6532-userdebug
-add_lunch_combo omni_Infinix-X6532-eng
+add_lunch_combo twrp_Infinix-X6532-user
+add_lunch_combo twrp_Infinix-X6532-userdebug
+add_lunch_combo twrp_Infinix-X6532-eng
