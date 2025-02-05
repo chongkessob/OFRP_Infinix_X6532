9d3127a0a61e8521000878f0c4220405b4f92820
chongkessob <chonks091004@gmail.com>
2025-02-05 13:14:26 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X6532
+LOCAL_PATH := device/infinix/Infinix_X6532
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -18,14 +18,24 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6768
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_PACKAGES:= \
     bootctrl.mt6768 \
     libgptutils \
     libz \
     libcutils
+
+# Google Deprecated this, as per statement from Google, this is not needed anymore
+# PRODUCT_STATIC_BOOT_CONTROL_HAL was the workaround to allow sideloading with statically 
+# linked boot control HAL, before shared library HALs were supported under recovery. 
+# Android Q has added such support (HALs will be loaded in passthrough mode), 
+# and the workarounds are being removed. Targets should build and install 
+# the recovery variant of boot control HAL modules into recovery image, similar to the ones 
+# installed for normal boot. 
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.mt6768 \
+#     libgptutils \
+#     libz \
+#     libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -33,3 +43,5 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
