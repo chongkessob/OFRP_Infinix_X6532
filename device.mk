63509dd111e695e36fae6a1ce427a68e97c4d04b
chongkessob <chonks091004@gmail.com>
2025-02-06 21:44:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix_X6532
+LOCAL_PATH := device/infinix/X6532
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
