b84e47aa0b9bdb41772f511e24773211ff53ad8d
chongkessob <chonks091004@gmail.com>
2025-02-07 19:01:06 +0800

    Update twrp_Infinix_X6532.mk

--- twrp_Infinix_X6532.mk
+++ twrp_Infinix_X6532.mk
@@ -5,31 +5,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := X6532
+
+DEVICE_PATH := device/infinix/$(PRODUCT_RELEASE_NAME)
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6532 device
-$(call inherit-product, device/infinix/Infinix_X6532/device.mk)
+$(call inherit-product, device/infinix/X6532/device.mk)
 
-##Define hardware platform
-PRODUCT_RELEASE_NAME := (Infinix_X5432)
-
-##Device path for OEM device tree
-DEVICE_PATH := (device/infinix/Infinix_X6532)
 
 ##Inherit any OrangeFox-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
 
-PRODUCT_DEVICE := Infinix_X6532
-PRODUCT_NAME := twrp_Infinix_X6532
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix_X6532
 PRODUCT_MANUFACTURER := infinix
+PRODUCT_MODEL := Infinix X6532
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_x6532_v632-user 12 SP1A.210812.016 722914 release-keys"
